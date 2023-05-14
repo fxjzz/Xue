ca12505801ea7696ec4ea70691f3494bbae35ec8
lzxzz <1042183935@qq.com>
2023-05-14 11:05:41 +0800

    feat: init handler

--- packages/reactivity/src/baseHandlers.ts
+++ packages/reactivity/src/baseHandlers.ts
@@ -1,9 +1,30 @@
-function get() {
-  return true
+const get = createGetter()
+function createGetter() {
+  return function get(target: object, key: string | symbol, receiver: object) {
+    const res = Reflect.get(target, key, receiver)
+
+    track()
+
+    return res
+  }
 }
-function set() {
-  return true
+
+const set = createSetter()
+function createSetter() {
+  return function set(
+    target: object,
+    key: string | symbol,
+    value: unknown,
+    receiver: object
+  ): boolean {
+    const result = Reflect.set(target, key, value, receiver)
+
+    trigger()
+
+    return result
+  }
 }
+
 function deleteProperty() {
   return false
 }
