dd47dd5fe1d0f8141f510c92238b7a9b162d54a7
lzxzz <1042183935@qq.com>
2023-05-28 16:51:07 +0800

    完成el的事件挂载和更新

---
+++ packages/runtime-dom/src/modules/event.ts
@@ -0,0 +1,35 @@
+export function patchEvent(
+  el: Element & { _vei?: Object },
+  rawName: string,
+  prevValue,
+  nextValue
+) {
+  const invokers = el._vei || (el._vei = {})
+
+  const existingInvoker = invokers[rawName]
+  if (nextValue && existingInvoker) {
+    existingInvoker.value = nextValue
+  } else {
+    const name = parseName(rawName)
+    if (nextValue) {
+      const invoker = (invokers[rawName] = createInvoker(nextValue))
+      el.addEventListener(name, invoker)
+    } else if (existingInvoker) {
+      el.removeEventListener(name, existingInvoker)
+      invokers[rawName] = undefined
+    }
+  }
+}
+
+function parseName(name: string) {
+  return name.slice(2).toLowerCase()
+}
+
+function createInvoker(initialValue) {
+  const invoker: any = (e: Event) => {
+    invoker.value && invoker.value()
+  }
+
+  invoker.value = initialValue
+  return invoker
+}
--- packages/runtime-dom/src/patchProp.ts
+++ packages/runtime-dom/src/patchProp.ts
@@ -1,10 +1,12 @@
 import { isOn } from '@xue/shared'
 import { patchClass } from './modules/class'
+import { patchEvent } from './modules/event'
 
 export const patchProp = (el: Element, key, prev, next) => {
   if (key === 'class') {
     patchClass(el, next)
   } else if (key === 'style') {
   } else if (isOn(key)) {
+    patchEvent(el, key, prev, next)
   }
 }
